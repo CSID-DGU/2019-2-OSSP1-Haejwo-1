f6eabed84f39aa7662956ea429c6896b47cf3585
sungwon <jsw2095@naver.com>
2019-10-01 13:00:25 +0900

    first

---
+++ haejwo-android/app/src/main/java/kr/rails/WebAppInterface.java
@@ -0,0 +1,22 @@
+package kr.rails;
+
+import android.content.Context;
+import android.webkit.JavascriptInterface;
+
+import kr.rails.MainActivity;
+
+public class WebAppInterface {
+    Context mContext;
+
+    public WebAppInterface(Context c) {
+        mContext = c;
+    }
+
+//    @SuppressWarnings("unused")
+////    @JavascriptInterface
+////    public void setUserId(String userId) {
+////        MainActivity mainActivity = (MainActivity) mContext;
+////        mainActivity.setUserId(userId);
+////    }
+}
+
