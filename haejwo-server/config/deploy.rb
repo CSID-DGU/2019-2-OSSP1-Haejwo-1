ed4581464c2c1f59f8e6b96e59e3a883ae9de80e
sungwon <jsw2095@naver.com>
2019-10-02 16:15:23 +0900

    capistrano git repo url reset

--- haejwo-server/config/deploy.rb
+++ haejwo-server/config/deploy.rb
@@ -4,7 +4,7 @@ lock "~> 3.11.0"
 
 
 set :application, "haejwo"
-set :repo_url, "git@github.com:godwon2095/haejwo-server.git"
+set :repo_url, "git@github.com:CSID-DGU/2019-2-OSSP1-Haejwo-1.git/haejwo-server"
 set :deploy_to, '/home/deploy/haejwo'
 
 append :linked_files, "config/master.key"
