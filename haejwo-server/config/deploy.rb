0711f6cb31e5d2caa13ff70d471b8dfa2a4e3447
sungwon <jsw2095@naver.com>
2019-10-02 16:42:21 +0900

    reset

--- haejwo-server/config/deploy.rb
+++ haejwo-server/config/deploy.rb
@@ -6,8 +6,8 @@ set :repo_url, 'git@github.com:CSID-DGU/2019-2-OSSP1-Haejwo-1.git'
 set :repo_tree, 'haejwo-server'
 set :deploy_to, '/home/deploy/haejwo'
 
-append :linked_files, %w{config/master.key}
-append :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+append :linked_files, 'config/master.key'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
 
 ## 배포 후 쓰이지 않는 gem 정리
 after 'deploy:published', 'bundler:clean'
