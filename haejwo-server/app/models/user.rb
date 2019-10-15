2b377f0bebbc8ed3dcd3a377e19333bc6deb53de
sungwon <jsw2095@naver.com>
2019-10-15 17:39:28 +0900

    for test

--- haejwo-server/app/models/user.rb
+++ haejwo-server/app/models/user.rb
@@ -32,9 +32,9 @@ class User < ApplicationRecord
         }
       }
       response = fcm.send(registration_ids, options)
-      msg = "푸시 결과 #{response.to_yaml}"
-      puts mgs
-      Rails.logger.info msg
+      # msg = "푸시 결과 #{response.to_yaml}"
+      # puts mgs
+      # Rails.logger.info msg
     end
   end
 end
