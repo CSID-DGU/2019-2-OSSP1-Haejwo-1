d81cbb871cade653d6b302b3132e4a38593352bb
sungwon <jsw2095@naver.com>
2019-10-29 13:45:25 +0900

    웹뷰 띄우는 기능 구현

--- haejwo-ios/haejwo-ios/ViewController.swift
+++ haejwo-ios/haejwo-ios/ViewController.swift
@@ -9,35 +9,92 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKNavigationDelegate {
-
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        // Do any additional setup after loading the view.
-//    }
-    var webView: WKWebView!
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
     
     override func loadView() {
-        webView = WKWebView()
+        super.loadView()
+        webView = WKWebView(frame: self.view.frame)
+        webView.uiDelegate = self
         webView.navigationDelegate = self
-        view = webView
+        self.view = self.webView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // 1
-        let url = URL(string: "https://rails.kr?platform=ios")!
-        webView.load(URLRequest(url: url))
-          
-        // 2
-        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
-        toolbarItems = [refresh]
-        navigationController?.isToolbarHidden = false
+        let url = URL(string: "https://rails.kr?platform=ios")
+        let request = URLRequest(url: url!)
+        webView.load(request)
     }
     
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        title = webView.title
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //alert 처리
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: {
+            (action) in
+            completionHandler()
+        }))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    //confirm 처리
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: {
+            (action) in
+            completionHandler(true)
+            
+        }))
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: {
+            (action) in
+            completionHandler(false)
+            
+        }))
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
+    //confirm 처리2
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping (String?) -> Void) {
+        let alertController = UIAlertController(title: "", message: prompt, preferredStyle: .alert)
+        alertController.addTextField {
+            (textField) in textField.text = defaultText
+        }
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: {(action) in
+            if let text = alertController.textFields?.first?.text {
+                completionHandler(text)
+            } else {
+                completionHandler(defaultText)
+            }
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: {
+            (action) in
+            completionHandler(nil)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    // href="_blank" 처리
+    func webView(_ webView: WKWebView, createWebViewWith configuration: WKWebViewConfiguration, for navigationAction: WKNavigationAction, windowFeatures: WKWindowFeatures) -> WKWebView? {
+        if navigationAction.targetFrame == nil {
+            webView.load(navigationAction.request)
+        }
+        return nil
+    }
+    
+    // 중복적으로 리로드 방지
+    public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        webView.reload()
     }
 }
 
