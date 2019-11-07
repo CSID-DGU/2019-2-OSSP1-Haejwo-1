2c5b989a6d007efd41c206385cf973655cf1ccc6
Juyeong Lee <2jy22@naver.com>
2019-11-08 04:15:54 +0900

    event delete fix

--- haejwo-server/app/models/chatroom.rb
+++ haejwo-server/app/models/chatroom.rb
@@ -1,7 +1,7 @@
 class Chatroom < ApplicationRecord
-  belongs_to :event
+  belongs_to :event, dependent: :destroy
   has_many :messages, dependent: :destroy
-  has_many :users, through: :messages
+  has_many :users, through: :messages, dependent: :destroy
 
   belongs_to :request_user, class_name: 'User'
   belongs_to :perform_user, class_name: 'User'
