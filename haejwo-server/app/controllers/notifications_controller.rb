34100c51a2cdb782c7f972491cb315484323115a
Juyeong Lee <2jy22@naver.com>
2019-11-09 23:21:02 +0900

    render chatroom list

--- haejwo-server/app/controllers/notifications_controller.rb
+++ haejwo-server/app/controllers/notifications_controller.rb
@@ -1,5 +1,5 @@
 class NotificationsController < ApplicationController
   def index
-    # @chatrooms = Chatroom.where('request_user_id = :user_id Or ')....
+    @chatrooms = Chatroom.where('request_user_id = :user_id OR perform_user_id = :user_id', user_id: current_user.id)
   end
 end
