4c10b73a8c1b9866987905ad085867a3b6fb5c66
sungwon <jsw2095@naver.com>
2019-12-16 20:05:42 +0900

    change list

--- haejwo-server/app/models/application_record.rb
+++ haejwo-server/app/models/application_record.rb
@@ -2,10 +2,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def enum_ko(column_name)
-    I18n.t("enum.#{self.class.name.downcase}.#{column_name}.#{self.send("#{column_name}")}")
+    I18n.t("enum.#{self.class.name.underscore}.#{column_name}.#{self.send("#{column_name}")}")
   end
 
   def self.enum_selectors(column_name)
-    I18n.t("enum.#{self.name.downcase}.#{column_name}").invert rescue []
+    I18n.t("enum.#{self.name.underscore}.#{column_name}").invert rescue []
   end
 end
