7f7bf356733b7d22c6a99deef911d6e94fde9273
sungwon <jsw2095@naver.com>
2019-12-16 20:47:17 +0900

    fix logic

--- haejwo-server/app/controllers/maps_controller.rb
+++ haejwo-server/app/controllers/maps_controller.rb
@@ -2,7 +2,8 @@ class MapsController < ApplicationController
   def index
     @buildings = Building.joins(:events)
                          .select('buildings.*, COUNT("events") matching_events_count')
-                         .where(events: {state: 'matching'})
+                         .where('events.state = ? AND events.time_limit >= ?', 'matching', Time.current)
                          .group('buildings.id')
   end
+  #.where(events: {state: 'matching'})
 end
