a570fdbf12beac7c3e0df1a49ffc496e3f930f89
sungwon <jsw2095@naver.com>
2019-12-17 13:53:04 +0900

    change

--- haejwo-server/app/services/search.rb
+++ haejwo-server/app/services/search.rb
@@ -1,17 +1,17 @@
 class Search
-  attr_reader :objs, :_sh, :q, :sort_params
+  attr_reader :objs, :_sh, :q, :sort_params1
 
-  def initialize(objs, _sh, q, sort_params)
+  def initialize(objs, _sh, q, sort_params1)
     @objs = objs
     @_sh = _sh
     @q = q
-    @sort_params = sort_params
+    @sort_params1 = sort_params1
   end
 
   def list
     r_objs = objs
     r_objs = _shed_objs(_sh) if (q.present? && _sh.present?)
-    r_objs = r_objs.order(sort_params)
+    r_objs = r_objs.order(sort_params1)
   end
 
   private
